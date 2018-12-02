37924ed972b3bfea17cf2ed53773b0451ef25c88
Aman Mathur <amanmathur99@gmail.com>
2018-12-02 17:33:52 -0500

    setting up chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { async } from 'rxjs/internal/scheduler/async';
+import io from 'socket.io-client';
+
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
+  constructor(props){
+    super(props)
 
-  componentDidMount(){
-    this.callApi()
-      .then(res => this.setState({response: res.express}))
-      .catch(err => console.log(err));
+    this.state = {
+    }
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+  componentDidMount(){
+    //Make socket connection
+    this.connectToSocket();
+  }
 
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
+  connectToSocket(){
+    var socket = io.connect('http://localhost:5000');
+  }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+      <div>
+        Test
       </div>
     );
   }
